8adeef1c60ad7dcce724865e8a8685bb9db3328f
Berzi441 <1145998608@qq.com>
2025-06-10 01:55:37 +0800

    添加文字缓冲变换=化

--- Assets/BattleSystem.cs
+++ Assets/BattleSystem.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public enum BattleState {
-    Start,PlayerTurn,EnemyTurn,Win,Lost
+public enum BattleState
+{
+    Start, PlayerTurn, EnemyTurn, Win, Lost
 }
 public class BattleSystem : MonoBehaviour
 {
@@ -25,14 +26,14 @@ public class BattleSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        state= BattleState.Start;
-        StartCoroutine( SetupBattle());
+        state = BattleState.Start;
+        StartCoroutine(SetupBattle());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private IEnumerator SetupBattle()
@@ -40,16 +41,17 @@ public class BattleSystem : MonoBehaviour
         GameObject enemy = Instantiate(enemyPrefab, emenyBattleStation);
         enemyPokemon = enemy.GetComponent<Pokemon>();
 
-        GameObject player = Instantiate(PlayerPrefab,playerBatteleStation);
+        GameObject player = Instantiate(PlayerPrefab, playerBatteleStation);
         playerPokemon = player.GetComponent<Pokemon>();
 
-        dialogText.text = "野生的" +  enemyPokemon.pokemonName + "跳出来了";
+        //dialogText.text = "野生的" + enemyPokemon.pokemonName + "跳出来了";
+        yield return StartCoroutine(TypeDialog("野生的" + enemyPokemon.pokemonName + "跳出来了"));
 
         playerHUD.SetHUD(playerPokemon);
         enemyHUD.SetHUD(enemyPokemon);
 
-        yield return new WaitForSeconds(3f);
-        if(enemyPokemon.spd > playerPokemon.spd)
+        yield return new WaitForSeconds(1f);
+        if (enemyPokemon.spd > playerPokemon.spd)
         {
             state = BattleState.EnemyTurn;
             StartCoroutine(EnemyTurn());
@@ -63,7 +65,8 @@ public class BattleSystem : MonoBehaviour
 
     private void PlayerTurn()
     {
-        dialogText.text = playerPokemon.pokemonName + "做什么";
+        //dialogText.text = playerPokemon.pokemonName + "做什么";
+         StartCoroutine(TypeDialog(playerPokemon.pokemonName + "做什么"));
     }
 
     public void OnAttackButton()
@@ -75,13 +78,15 @@ public class BattleSystem : MonoBehaviour
 
     private IEnumerator PlayerAttack()
     {
-        dialogText.text = playerPokemon.pokemonName + "使用了攻击！";
-        yield return new WaitForSeconds(3f);
-        int dmg = enemyPokemon.TakeDamage(playerPokemon.atk, enemyPokemon.def,playerPokemon.property,enemyPokemon.property);
-        dialogText.text = enemyPokemon.pokemonName + "受到了" + dmg + "点伤害";
+        //dialogText.text = playerPokemon.pokemonName + "使用了攻击！";
+        yield return StartCoroutine(TypeDialog(playerPokemon.pokemonName + "使用了攻击！"));
+        yield return new WaitForSeconds(1f);
+        int dmg = enemyPokemon.TakeDamage(playerPokemon.atk, enemyPokemon.def, playerPokemon.property, enemyPokemon.property);
+        //dialogText.text = enemyPokemon.pokemonName + "受到了" + dmg + "点伤害";
+        yield return StartCoroutine(TypeDialog(enemyPokemon.pokemonName + "受到了" + dmg + "点伤害"));
         bool isDefeated = enemyPokemon.isDefeated(dmg);
         enemyHUD.SetHP(enemyPokemon.currentHP);
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(1f);
         if (isDefeated)
         {
             state = BattleState.Win;
@@ -94,27 +99,31 @@ public class BattleSystem : MonoBehaviour
         }
     }
 
-    private void EndBattle()
+    private IEnumerator EndBattle()
     {
         if (state == BattleState.Win)
         {
-            dialogText.text = playerPokemon.pokemonName + "赢得了战斗！";
+            //dialogText.text = playerPokemon.pokemonName + "赢得了战斗！";
+            yield return StartCoroutine(TypeDialog(playerPokemon.pokemonName + "赢得了战斗！"));
         }
         else
         {
-            dialogText.text = playerPokemon.pokemonName + "被击败了！";
+            //dialogText.text = playerPokemon.pokemonName + "被击败了！";
+            yield return StartCoroutine(TypeDialog(playerPokemon.pokemonName + "被击败了！"));
         }
     }
 
     private IEnumerator EnemyTurn()
     {
-        dialogText.text = enemyPokemon.pokemonName + "使用了攻击！";
-        yield return new WaitForSeconds(2f);
+        //dialogText.text = enemyPokemon.pokemonName + "使用了攻击！";
+        yield return StartCoroutine(TypeDialog(enemyPokemon.pokemonName + "使用了攻击！"));
+        yield return new WaitForSeconds(1f);
         int dmg = playerPokemon.TakeDamage(enemyPokemon.atk, playerPokemon.def, enemyPokemon.property, playerPokemon.property);
-        dialogText.text = playerPokemon.pokemonName + "受到了" + dmg + "点伤害";
+        //dialogText.text = playerPokemon.pokemonName + "受到了" + dmg + "点伤害";
+        yield return StartCoroutine(TypeDialog(playerPokemon.pokemonName + "受到了" + dmg + "点伤害"));
         bool isDefeated = playerPokemon.isDefeated(dmg);
         playerHUD.SetHP(playerPokemon.currentHP);
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
         if (isDefeated)
         {
             state = BattleState.Lost;
@@ -138,11 +147,22 @@ public class BattleSystem : MonoBehaviour
     {
         playerPokemon.Heal(playerPokemon.pokemonLevel);
         playerHUD.SetHP(playerPokemon.currentHP);
-        dialogText.text = playerPokemon.pokemonName + "使用了治疗！";
+        //dialogText.text = playerPokemon.pokemonName + "使用了治疗！";
+        yield return StartCoroutine(TypeDialog(playerPokemon.pokemonName + "使用了治疗！"));
         yield return new WaitForSeconds(1f);
-        dialogText.text = playerPokemon.pokemonName + "的体力恢复了！";
-        yield return new WaitForSeconds(2f);
+        //dialogText.text = playerPokemon.pokemonName + "的体力恢复了！";
+        yield return StartCoroutine(TypeDialog(playerPokemon.pokemonName + "的体力恢复了！"));
+        yield return new WaitForSeconds(1f);
         state = BattleState.EnemyTurn;
         StartCoroutine(EnemyTurn());
+    }
+    private IEnumerator TypeDialog(string dialog)
+    {
+        dialogText.text = "";
+
+        foreach (var item in dialog) { 
+            dialogText.text += item;
+            yield return new WaitForSeconds(1f/30);
+        }
     }
 }
