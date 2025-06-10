2175fca22a6075b6aa3b70dfd91ed22e53d5db82
Berzi441 <1145998608@qq.com>
2025-06-11 00:51:23 +0800

    修复了多次按下按钮会进行多次事件导致文字逐字显示异常的问题

--- Assets/BattleSystem.cs
+++ Assets/BattleSystem.cs
@@ -16,7 +16,8 @@ public class BattleSystem : MonoBehaviour
     public Transform playerBatteleStation;
     public Transform emenyBattleStation;
     public TextMeshProUGUI dialogText;
-
+    public Button attackButton;
+    public Button healButton;
     private Pokemon playerPokemon;
     private Pokemon enemyPokemon;
 
@@ -47,10 +48,10 @@ public class BattleSystem : MonoBehaviour
         //dialogText.text = "野生的" + enemyPokemon.pokemonName + "跳出来了";
         yield return StartCoroutine(TypeDialog("野生的" + enemyPokemon.pokemonName + "跳出来了"));
 
+
         playerHUD.SetHUD(playerPokemon);
         enemyHUD.SetHUD(enemyPokemon);
 
-        yield return new WaitForSeconds(1f);
         if (enemyPokemon.spd > playerPokemon.spd)
         {
             state = BattleState.EnemyTurn;
@@ -59,44 +60,45 @@ public class BattleSystem : MonoBehaviour
         else
         {
             state = BattleState.PlayerTurn;
-            PlayerTurn();
+            StartCoroutine(PlayerTurn());
         }
     }
-
-    private void PlayerTurn()
+    private bool isActionInProgress = false;
+    private IEnumerator PlayerTurn()
     {
         //dialogText.text = playerPokemon.pokemonName + "做什么";
-         StartCoroutine(TypeDialog(playerPokemon.pokemonName + "做什么"));
+        yield return StartCoroutine(TypeDialog(playerPokemon.pokemonName + "做什么?"));
     }
 
     public void OnAttackButton()
     {
-        if (state != BattleState.PlayerTurn)
+        if (state != BattleState.PlayerTurn || isActionInProgress) 
             return;
+        isActionInProgress = true;
+        attackButton.interactable = false;
         StartCoroutine(PlayerAttack());
     }
-
     private IEnumerator PlayerAttack()
     {
         //dialogText.text = playerPokemon.pokemonName + "使用了攻击！";
         yield return StartCoroutine(TypeDialog(playerPokemon.pokemonName + "使用了攻击！"));
-        yield return new WaitForSeconds(1f);
         int dmg = enemyPokemon.TakeDamage(playerPokemon.atk, enemyPokemon.def, playerPokemon.property, enemyPokemon.property);
         //dialogText.text = enemyPokemon.pokemonName + "受到了" + dmg + "点伤害";
         yield return StartCoroutine(TypeDialog(enemyPokemon.pokemonName + "受到了" + dmg + "点伤害"));
         bool isDefeated = enemyPokemon.isDefeated(dmg);
         enemyHUD.SetHP(enemyPokemon.currentHP);
-        yield return new WaitForSeconds(1f);
         if (isDefeated)
         {
             state = BattleState.Win;
-            EndBattle();
+            StartCoroutine(EndBattle());
         }
         else
         {
             state = BattleState.EnemyTurn;
             StartCoroutine(EnemyTurn());
         }
+        isActionInProgress = false;
+        attackButton.interactable = true;
     }
 
     private IEnumerator EndBattle()
@@ -117,29 +119,29 @@ public class BattleSystem : MonoBehaviour
     {
         //dialogText.text = enemyPokemon.pokemonName + "使用了攻击！";
         yield return StartCoroutine(TypeDialog(enemyPokemon.pokemonName + "使用了攻击！"));
-        yield return new WaitForSeconds(1f);
         int dmg = playerPokemon.TakeDamage(enemyPokemon.atk, playerPokemon.def, enemyPokemon.property, playerPokemon.property);
         //dialogText.text = playerPokemon.pokemonName + "受到了" + dmg + "点伤害";
         yield return StartCoroutine(TypeDialog(playerPokemon.pokemonName + "受到了" + dmg + "点伤害"));
         bool isDefeated = playerPokemon.isDefeated(dmg);
         playerHUD.SetHP(playerPokemon.currentHP);
-        yield return new WaitForSeconds(1f);
         if (isDefeated)
         {
             state = BattleState.Lost;
-            EndBattle();
+            StartCoroutine(EndBattle());
         }
         else
         {
             state = BattleState.PlayerTurn;
-            PlayerTurn();
+            StartCoroutine(PlayerTurn());
         }
     }
 
     public void OnHealButton()
     {
-        if (state != BattleState.PlayerTurn)
+        if (state != BattleState.PlayerTurn || isActionInProgress)
             return;
+        isActionInProgress = true;
+        healButton.interactable = false;
         StartCoroutine(PlayerHeal());
     }
 
@@ -149,20 +151,22 @@ public class BattleSystem : MonoBehaviour
         playerHUD.SetHP(playerPokemon.currentHP);
         //dialogText.text = playerPokemon.pokemonName + "使用了治疗！";
         yield return StartCoroutine(TypeDialog(playerPokemon.pokemonName + "使用了治疗！"));
-        yield return new WaitForSeconds(1f);
         //dialogText.text = playerPokemon.pokemonName + "的体力恢复了！";
         yield return StartCoroutine(TypeDialog(playerPokemon.pokemonName + "的体力恢复了！"));
-        yield return new WaitForSeconds(1f);
         state = BattleState.EnemyTurn;
-        StartCoroutine(EnemyTurn());
+        yield return StartCoroutine(EnemyTurn());
+        isActionInProgress = false;
+        healButton.interactable = true;
     }
     private IEnumerator TypeDialog(string dialog)
     {
         dialogText.text = "";
 
-        foreach (var item in dialog) { 
+        foreach (var item in dialog)
+        {
             dialogText.text += item;
-            yield return new WaitForSeconds(1f/30);
+            yield return new WaitForSeconds(1f / 30);
         }
+        yield return new WaitForSeconds(1f);
     }
 }
